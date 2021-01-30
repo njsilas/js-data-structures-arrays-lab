9514489e09f288519579de3ea084f0a12343d661
Nicholas Silas <njsilas@gmail.com>
2021-01-30 17:15:29 -0500

    commit tree b2d00028e105a3222076ee1effc461044c93961e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver (snax) {
+    drivers.push(snax);
+  }
+function destructivelyPrependDriver (gem) {
+    drivers.unshift(gem);
+}
+function destructivelyRemoveLastDriver () {
+    drivers.pop();
+}
+function destructivelyRemoveFirstDriver () {
+    drivers.shift();
+}
+function appendDriver (gimme) {
+return [...drivers, gimme];
+}
+function prependDriver (smash) {
+return [smash, ...drivers];
+}
+function removeFirstDriver () {
+    return drivers.slice(1);
+}
+function removeLastDriver() {
+return drivers.slice(0, drivers.length - 1);
+}
